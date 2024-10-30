4905b20b40e94a7f258fd99917750dce1c6b9399
Juan Caceres <Juandc760@gmail.com>
2024-10-30 16:57:08 -0500

    feat: add auto animate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/ui'],
+  modules: ['@nuxt/ui', '@formkit/auto-animate/nuxt'],
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true }
 })
