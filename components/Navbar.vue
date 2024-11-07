e61b8d61a7e7a92c95bda3928732ec93a7f14ac7
Juan Caceres <Juandc760@gmail.com>
2024-11-07 16:58:24 -0500

    feat: navbar improvement

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,9 +2,19 @@
     <nav class="flex justify-between items-center p-4">
         <UButton color="primary" to="/">Home</UButton>
         <div class="flex space-x-2">
-            <UButton color="primary" to="/todo">Todo list</UButton>
-            <UButton color="primary" to="/templates">Templates</UButton>
-            <UButton color="primary" to="/processes">Processes</UButton>    
+            <UButton v-for="link in links" :key="link.to" :to="link.to" :label="link.label" />
         </div>
     </nav>
 </template>
+
+<script setup>
+const links = [
+    { label: 'Todo list', to: '/todo' },
+    { label: 'Templates', to: '/templates' },
+    { label: 'Processes', to: '/processes' },
+    { label: 'Key Information', to: '/key-info' },
+
+]
+
+</script>
+
