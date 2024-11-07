185f754907a52e23843b8942558f6ef6063d4e1e
Juan Caceres <Juandc760@gmail.com>
2024-11-07 16:59:37 -0500

    feat: add new key info page

---
+++ pages/key-info/[slug].vue
@@ -0,0 +1,26 @@
+<template>
+    <h1 class="text-center text-2xl font-bold">{{ keyInfo.title }}</h1>
+    <img :src="getImageUrl(keyInfo.image)" alt="Key Information Image" class="w-5/6 h-auto mx-auto">
+    <div v-html="keyInfo.content" class="table-auto"></div>
+ 
+
+    
+</template>
+
+
+
+
+
+<script setup>
+const route = useRoute()
+const slug = route.params.slug
+const pb = usePocketBase()
+const keyInfo = await pb.collection('key_info').getFirstListItem(`slug = "${slug}"`)
+
+// Function to get the full image URL
+const getImageUrl = (fileName) => {
+    return `${pb.baseUrl}/api/files/key_info/${keyInfo.id}/${fileName}`
+}
+</script>
+
+
---
+++ pages/key-info/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <UContainer>
+        <h1 class="text-center text-2xl font-bold">Key Information</h1>
+        <ul class="space-y-4">
+        <li v-for="keyInfo in keyInfoList" :key="keyInfo.id" class="border rounded-lg p-4 shadow-sm hover:shadow-md transition-shadow">
+            <UButton :to="`/key-info/${keyInfo.slug}`">{{ keyInfo.title }}</UButton>
+            </li>
+        </ul>
+    </UContainer>
+</template>
+
+
+<script setup>
+const pb = usePocketBase()
+const keyInfoList = await pb.collection('key_info').getFullList()
+</script>
